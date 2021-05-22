1c369545f74e9b8557e0dda53c3e03aad19e490d
naumi421 <tnauman@usgs.gov>
2021-05-22 14:33:35 -0600

    Switched to QRMSE in transformed units

--- R/validation_stats.R
+++ R/validation_stats.R
@@ -22,7 +22,7 @@ valmetrics <- function(xlst, trans, varrange, prop, depth){
     model <- paste(prop,depth,"cm",sep="_")
     valtype <- pts.extpcv$valtype[1]
     varrange <- as.numeric(quantile(pts.extpcv$prop, probs=c(0.975), na.rm=T)-quantile(pts.extpcv$prop, probs=c(0.025),na.rm=T))
-    qrtrange <- as.numeric(quantile(pts.extpcv$prop, probs=c(0.75), na.rm=T)-quantile(pts.extpcv$prop, probs=c(0.25),na.rm=T))
+    qrtrange <- as.numeric(quantile(pts.extpcv$prop_t, probs=c(0.75), na.rm=T)-quantile(pts.extpcv$prop_t, probs=c(0.25),na.rm=T))
     ## CV statistics: all data
     n <- length(pts.extpcv[,1])
     RMSE <- sqrt(mean((pts.extpcv$prop_t - pts.extpcv$pcvpred)^2, na.rm=TRUE))
@@ -30,6 +30,8 @@ valmetrics <- function(xlst, trans, varrange, prop, depth){
     if("foldRsq"%in%colnames(pts.extpcv)){Rsqvar <- var(unique(pts.extpcv$foldRsq), na.rm=TRUE)}else{Rsqvar <- 'NA'}
     Rsqpre <- 1-var(pts.extpcv$prop_t - pts.extpcv$pcvpredpre, na.rm=TRUE)/var(pts.extpcv$prop_t, na.rm=TRUE)
     Bias <- mean(pts.extpcv$prop_t - pts.extpcv$pcvpred, na.rm=TRUE)/mean(pts.extpcv$prop_t, na.rm=T)
+    QRMSE <- qrtrange / sqrt(mean((pts.extpcv$prop_t - pts.extpcv$pcvpred)^2, na.rm=TRUE))
+    QMedAE <- qrtrange / median(abs(pts.extpcv$prop_t - pts.extpcv$pcvpred), na.rm=TRUE)
     ## Back transformed: create pcvpred_bt even if not tranformed for cv.depth function: Using Duan's smearing estimator
     if(trans=="log10") {pts.extpcv$pcvpred_bt <- (10^(pts.extpcv$pcvpred) - 0.1)*(mean(10^(pts.extpcv$prop_t - pts.extpcv$pcvpred)))}
     if(trans=="log") {pts.extpcv$pcvpred_bt <- (exp(pts.extpcv$pcvpred) - 1)*(mean(exp(pts.extpcv$prop_t - pts.extpcv$pcvpred)))}
@@ -41,8 +43,6 @@ valmetrics <- function(xlst, trans, varrange, prop, depth){
     MAE_bt <- mean(abs(pts.extpcv$prop - pts.extpcv$pcvpred_bt), na.rm=TRUE) # Mean Absolute Accuracy
     MedAE_bt <- median(abs(pts.extpcv$prop - pts.extpcv$pcvpred_bt), na.rm=TRUE) # Median Absolute Accuracy
     Bias_bt <- mean(pts.extpcv$prop - pts.extpcv$pcvpred_bt, na.rm=TRUE)/mean(pts.extpcv$prop_t, na.rm=T)
-    QRMSE_bt <- qrtrange / sqrt(mean((pts.extpcv$prop - pts.extpcv$pcvpred_bt)^2, na.rm=TRUE))
-    QMedAE_bt <- qrtrange / median(abs(pts.extpcv$prop - pts.extpcv$pcvpred_bt), na.rm=TRUE)
     ## RPI
     ## Back transform low PI
     # TODO Not sure if smearing estimator should be used on PIs? Probably not since the linear
@@ -69,7 +69,7 @@ valmetrics <- function(xlst, trans, varrange, prop, depth){
     RPI.cvmed <- median(pts.extpcv$RPI)
     PICP <- sum(ifelse(pts.extpcv$prop_bt <= pts.extpcv$pcvpredpre.975_bt & pts.extpcv$prop_bt >= pts.extpcv$pcvpredpre.025_bt,1,0))/length(pts.extpcv[,1])
     ## Create CV statistics table
-    CVdf <- data.frame(model, valtype, n, RMSE, Rsq, Rsqpre, Bias, RMSE_bt, Rsq_bt, MAE_bt, MedAE_bt, Bias_bt, QRMSE_bt, QMedAE_bt, RPI.cvave,RPI.cvmed,PICP)
+    CVdf <- data.frame(model, valtype, n, RMSE, Rsq, Rsqpre, Bias, QRMSE, QMedAE, RMSE_bt, Rsq_bt, MAE_bt, MedAE_bt, Bias_bt, RPI.cvave,RPI.cvmed,PICP)
     if("cvgrid" %in% colnames(pts.extpcv)){CVdf$cvgrid <- pts.extpcv$cvgrid[1]}
     return(CVdf)
   }
