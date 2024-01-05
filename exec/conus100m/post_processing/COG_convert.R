df757862c1cbc204479e5e7202a8086e632fce6b
naumi421 <naumi421@gmail.com>
2024-01-05 23:43:33 +0000

    COG creation

---
+++ exec/conus100m/post_processing/COG_convert.R
@@ -0,0 +1,43 @@
+
+# Workspace setup
+# Install packages if not already installed
+required.packages <- c("raster", "doParallel","gdalraster") #"gdalUtilities"
+new.packages <- required.packages[!(required.packages %in% installed.packages()[,"Package"])]
+if(length(new.packages)) install.packages(new.packages)
+lapply(required.packages, require, character.only=T)
+rm(required.packages, new.packages)
+## Increase actuve memory useable by raster package
+# memory.limit(100000)
+rasterOptions(maxmemory = 6.5e+09,chunksize = 5e+08, memfrac = 0.9)
+
+cpus <- min(124,detectCores() - 2)
+
+## Folders
+infldr <- "/mnt/disks/sped/solus100preds/public_layers"
+outfldr <- "/mnt/disks/sped/solus100preds/public_layers_cog"
+
+## Files and index
+infiles <- list.files(path = infldr,pattern=".tif*",full.names = T, recursive = T)
+rlist <- 1:length(infiles)
+
+cog_fn <- function(r){
+  rast <- raster(infiles[r])
+  fullpath <- infiles[r]
+  #src_dataset <- system.file(fullpath, package="gdalUtils")
+  flnm <- basename(infiles[r])
+  dtype <- dataType(rast)
+  if(dtype == "INT1U"){dtypegdal <- "Byte"}
+  if(dtype == "INT2U"){dtypegdal <- "UInt16"}
+  if(dtype == "FLT4S"){dtypegdal <- "Float32"}
+  args <- c("-ot", dtypegdal)
+  args <- c(args, "-of", "COG", "-co", "COMPRESSED=YES")
+  translate(fullpath, file.path(outfldr,flnm), args)
+  #gdal_translate(fullpath,file.path(outfldr,flnm),of="COG", ot = dtypegdal)
+}
+
+dtlist <- c("INT1U")
+
+for(f in infiles){
+  dt <- dataType(raster(f))
+  dtlist <- append(dtlist,dt)
+}
